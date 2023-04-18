01b46acb3c8d3276c2bbae7ddc778340a8a85708
Oco <nachodecores@hotmail.com>
2023-04-18 13:04:14 -0300

    Initial setup

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./partials";
+import Hero from "./partials";
+import Auction from "./partials";
+import Partners from "./partials";
+import Footer from "./partials";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Auction />
+      <Partners />
+      <Footer />
+    </>
+  );
+}
