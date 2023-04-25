af378640f2db139b49804c3721456e2382733787
Oco <nachodecores@hotmail.com>
2023-04-25 18:11:41 -0300

    fhsfgh

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import Header from "./partials/Header";
 import Hero from "./partials/Hero";
 import Poster from "./partials/Poster";
+import Footer from "./partials/Footer";
 // import Partners from "./partials";
-// import Footer from "./partials";
 
 export default function Home() {
   return (
@@ -11,8 +11,8 @@ export default function Home() {
       <Header />
       <Hero />
       <Poster />
-      {/* <Partners />
-      <Footer /> */}
+      {/* <Partners /> */}
+      <Footer />
     </>
   );
 }
---
+++ src/components/partials/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer d-flex align-items-center">
+      <img
+        id="logo-footer"
+        src="img/logo-pantalladelsur-claro.jpg"
+        type=""
+        alt="logo Pantall del Sur"
+      ></img>
+      <Link className="" to="/login">
+        LogIn
+      </Link>
+    </div>
+  );
+}
