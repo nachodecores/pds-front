5e46a8e31cf306f22643ad2857495a7b9aceab98
Oco <nachodecores@hotmail.com>
2023-04-26 11:04:38 -0300

    fdafdas

--- src/components/Auction.jsx
+++ src/components/Auction.jsx
@@ -3,18 +3,6 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 export default function Auction() {
-  const [categories, setCategories] = useState([]);
-  useEffect(() => {
-    const getCategories = async () => {
-      const response = await axios({
-        method: "GET",
-        url: `${process.env.REACT_APP_API_URL}/categories`,
-      });
-      setCategories(response.data);
-    };
-    getCategories();
-  }, []);
-
   return (
     <>
       <h1>Remate</h1>
