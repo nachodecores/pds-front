6f6f8ebd5306c96b260fe97c76b5caadc7b7fb1d
Oco <92460234+nachodecores@users.noreply.github.com>
2023-05-25 19:31:56 -0300

    card detalle

--- src/components/partials/HerdCard.jsx
+++ src/components/partials/HerdCard.jsx
@@ -6,37 +6,35 @@ import "../styles/HerdCard.css";
 export default function HerdCard({ herd }) {
   return (
     <>
-      <Link to={`/catalogo/${herd.id}`}>
-        <article>
-          <figure className="card-image">
-            <img src="img/vacaslecheras.jpeg" alt="Video del lote" />
-          </figure>
-          <div className="card-header">
-            {herd.quantity} {herd.category} vacas {herd.breed}
-          </div>
-          <div className="card-meta">Villa Rodríguez, San José</div>
-          <div className="card-footer">
-            <Link to="/catalogo">
-              <Button variant="primary">Más Información</Button>
-            </Link>
-            <button className="icon-button">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                display="block"
-                id="Heart"
-              >
-                <path d="M7 3C4.239 3 2 5.216 2 7.95c0 2.207.875 7.445 9.488 12.74a.985.985 0 0 0 1.024 0C21.125 15.395 22 10.157 22 7.95 22 5.216 19.761 3 17 3s-5 3-5 3-2.239-3-5-3z" />
-              </svg>
-            </button>
-          </div>
-        </article>
-      </Link>
+      <article>
+        <figure className="card-image">
+          <img src="img/vacaslecheras.jpeg" alt="Video del lote" />
+        </figure>
+        <div className="card-header">
+          {herd.quantity} {herd.category} vacas {herd.breed}
+        </div>
+        <div className="card-meta">Villa Rodríguez, San José</div>
+        <div className="card-footer">
+          <Link to={`/catalogo/${herd.id}`}>
+            <Button variant="primary">Más Información</Button>
+          </Link>
+          <button className="icon-button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              display="block"
+              id="Heart"
+            >
+              <path d="M7 3C4.239 3 2 5.216 2 7.95c0 2.207.875 7.445 9.488 12.74a.985.985 0 0 0 1.024 0C21.125 15.395 22 10.157 22 7.95 22 5.216 19.761 3 17 3s-5 3-5 3-2.239-3-5-3z" />
+            </svg>
+          </button>
+        </div>
+      </article>
     </>
   );
 }
