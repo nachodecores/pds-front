70412030938652f1dcda2971c740b9a280f65bb7
Oco <92460234+nachodecores@users.noreply.github.com>
2023-05-25 09:37:01 -0300

    cambio en la card de herd (lote)

--- src/components/partials/HerdCard.jsx
+++ src/components/partials/HerdCard.jsx
@@ -1,35 +1,42 @@
 import React from "react";
-import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
 import { Link } from "react-router-dom";
+import "../styles/HerdCard.css";
 
 export default function HerdCard({ herd }) {
-  const handleOpen = () => alert("link a detalle del lote");
-
   return (
     <>
       <Link to={`/catalogo/${herd.id}`}>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardActionArea onClick={handleOpen}>
-            <CardMedia
-              component="img"
-              height="140"
-              image="img/vacaslecheras.jpeg"
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {herd.quantity} {herd.category} vacas {herd.breed}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {herd.location} Puntas de Valdez, San José
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
+        <div class="card-list">
+          <article class="card">
+            <figure class="card-image">
+              <img src="img/vacaslecheras.jpeg" alt="Video del lote" />
+            </figure>
+            <div class="card-header">
+              {herd.quantity} {herd.category} vacas {herd.breed}
+              <button class="icon-button">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  display="block"
+                  id="Heart"
+                >
+                  <path d="M7 3C4.239 3 2 5.216 2 7.95c0 2.207.875 7.445 9.488 12.74a.985.985 0 0 0 1.024 0C21.125 15.395 22 10.157 22 7.95 22 5.216 19.761 3 17 3s-5 3-5 3-2.239-3-5-3z" />
+                </svg>
+              </button>
+            </div>
+            <div class="card-footer">
+              <div class="card-meta card-meta--views">{herd.location}</div>
+              <div class="card-meta card-meta--date">Jul 26, 2019</div>
+            </div>
+          </article>
+        </div>
       </Link>
     </>
   );
