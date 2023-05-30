e06ad85d7e7e19c8119a7e98eba101c634dafd89
Oco <92460234+nachodecores@users.noreply.github.com>
2023-05-30 15:14:21 -0300

    estilos

--- src/components/HerdDetail.jsx
+++ src/components/HerdDetail.jsx
@@ -3,39 +3,22 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import Header from "./partials/Header";
-
+import { Button } from "react-bootstrap";
 import CropSquareRoundedIcon from "@mui/icons-material/CropSquareRounded";
 import SquareRoundedIcon from "@mui/icons-material/SquareRounded";
 import Rating from "@mui/material/Rating";
 import "./styles/HerdDetail.css";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-import IconButton from "@mui/material/IconButton";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+function handlePreBid() {
+  alert("Lógica de las preofertas");
+}
 
 export default function HerdDetail() {
   const [herd, setHerd] = useState({});
   const [auctioneer, setAuctioneer] = useState([]);
   const { id } = useParams();
-
-  const [expanded, setExpanded] = React.useState(false);
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
 
   const StyledRating = styled(Rating)({
     "& .MuiRating-iconFilled": {
@@ -73,85 +56,81 @@ export default function HerdDetail() {
   return (
     <>
       <Header />
-      <Card className="herd-detail-card">
-        <div className="herd-detail-card-header">
-          <div className="left-side">
-            <img
-              src={`../img/${auctioneer.logo}`}
-              alt=""
-              className="logo-auctioneer"
-            />
-            <div>
-              <h1>
-                {herd.quantity} vacas de invernada {herd.breed}
-              </h1>
-              <h4>Villa Rodríguez, San José</h4>
+      <div className="container">
+        <div className="herd-detail-card card">
+          <div className="herd-detail-card-header">
+            <div className="left-side">
+              <img
+                src={`../img/${auctioneer.logo}`}
+                alt=""
+                className="logo-auctioneer"
+              />
+              <div>
+                <h1>
+                  {herd.quantity} vacas de invernada {herd.breed}
+                </h1>
+              </div>
             </div>
+            <button className="icon-button">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                display="block"
+                id="Heart"
+              >
+                <path d="M7 3C4.239 3 2 5.216 2 7.95c0 2.207.875 7.445 9.488 12.74a.985.985 0 0 0 1.024 0C21.125 15.395 22 10.157 22 7.95 22 5.216 19.761 3 17 3s-5 3-5 3-2.239-3-5-3z" />
+              </svg>
+            </button>
           </div>
-          <button className="icon-button">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              display="block"
-              id="Heart"
-            >
-              <path d="M7 3C4.239 3 2 5.216 2 7.95c0 2.207.875 7.445 9.488 12.74a.985.985 0 0 0 1.024 0C21.125 15.395 22 10.157 22 7.95 22 5.216 19.761 3 17 3s-5 3-5 3-2.239-3-5-3z" />
-            </svg>
-          </button>
-        </div>
-        <div className="card-media"></div>
-        <div>
-          <p>{herd.description}</p>
-        </div>
-        <div className="card-actions"></div>
-        <CardActions disableSpacing>
-          <IconButton aria-label="share">
-            <ShareIcon />
-          </IconButton>
-          <ExpandMore
-            expand={expanded}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-          >
-            <ExpandMoreIcon />
-          </ExpandMore>
-        </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <div className="card-media"></div>
           <div>
-            <p>Vendedor: Ignacio de Cores</p>
-            <div className="ratings">
-              <div className="class-type">
-                <h3>CLASE</h3>
-                <StyledRating
-                  name="customized-color"
-                  defaultValue={0}
-                  precision={1}
-                  icon={<SquareRoundedIcon fontSize="inherit" />}
-                  emptyIcon={<CropSquareRoundedIcon fontSize="inherit" />}
-                />
-              </div>
-              <div className="status">
-                <h3>ESTADO</h3>
-                <StyledRating
-                  name="customized-color"
-                  defaultValue={0}
-                  precision={1}
-                  icon={<SquareRoundedIcon fontSize="inherit" />}
-                  emptyIcon={<CropSquareRoundedIcon fontSize="inherit" />}
-                />
-              </div>
-            </div>
-            <p>Peso estimado</p>
-            <p>Precio</p>
+            <h4>{herd.description}</h4>
           </div>
-        </Collapse>
-      </Card>
+        </div>
+
+        <div className="seller">
+          <h3>Vendedor: Ignacio de Cores</h3>
+          <h3>Ubicación: Villa Rodríguez, San José</h3>
+        </div>
+
+        <div className="details">
+          <div className="values card">
+            <h4>Peso estimado: 160 kg</h4>
+            <h4>Precio: 2.15 U$S/kg</h4>
+          </div>
+          <div className="ratings card">
+            <div className="class-type d-flex">
+              <h4>CLASE</h4>
+              <StyledRating
+                name="customized-color"
+                defaultValue={0}
+                precision={1}
+                icon={<SquareRoundedIcon fontSize="inherit" />}
+                emptyIcon={<CropSquareRoundedIcon fontSize="inherit" />}
+              />
+            </div>
+            <div className="status d-flex">
+              <h4>ESTADO</h4>
+              <StyledRating
+                name="customized-color"
+                defaultValue={0}
+                precision={1}
+                icon={<SquareRoundedIcon fontSize="inherit" />}
+                emptyIcon={<CropSquareRoundedIcon fontSize="inherit" />}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="prebid">
+          <h3>Precio preofertado: 2,18 U$S/kg</h3>
+          <Button onClick={() => handlePreBid()}>Preofertar</Button>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/partials/Poster.jsx
+++ src/components/partials/Poster.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 import "../styles/Poster.css";
-import { Button, Card } from "react-bootstrap";
 
 export default function Poster() {
   return (
     <div className="poster">
       <Link to="/remate">Remate en vivo</Link>
-
       <div className="card container m-5">
         <h3>Próximo Remate</h3>
-        <Card.Body>
+        <div>
           <h2>MARTES 28 DE AGOSTO</h2>
-          <Card.Text>Local de feria de Juan Soler</Card.Text>
+          <p>Local de feria de Juan Soler</p>
           <Link to="/catalogo">
             <Button>Ver Catálogo</Button>
           </Link>
-        </Card.Body>
+        </div>
       </div>
     </div>
   );
